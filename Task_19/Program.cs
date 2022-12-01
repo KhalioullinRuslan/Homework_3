6ee05875021b1c551e296fe8b016f902b94181bb
unknown <xarus82@mail.ru>
2022-12-01 14:46:03 +0300

    Задача на определение палиндрома

---
+++ Task_19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+int number = 123454321;
+
+string strNumber = Convert.ToString(number);
+char[] numberCharArray = strNumber.ToCharArray();
+Array.Reverse(numberCharArray);
+string strReverseNumber = new string (numberCharArray);
+int revNumber = Convert.ToInt32(strReverseNumber);
+
+if (number == revNumber)
+   Console.WriteLine($"Число {number} - является палиндромом");
+else 
+   Console.WriteLine($"Число {number} - не является полиндромом");
+
+
